5369f4d7f30fc0e7ab433666445d7b45ee6b29b2
Amy Lim H L <66409683+Ocean272@users.noreply.github.com>
2022-03-31 21:22:18 +0800

    Edited 31_Mar_22_2122_Amy_LimHL

--- src/components/AgeList.js
+++ src/components/AgeList.js
@@ -11,15 +11,15 @@ const AgeList = ({ add }) => {
     <div>
         <h4>Covid cases by Age</h4>
         <form>
-            <label for="age_group">Select age group : </label>
-            <select value={age} onChange={(e) => setAge(e.target.value)}>
-              <item value="70 years old and above">70 years old and above</item>
-              <item value="60 - 69 years old">60 - 69 years old</item>
-              <item value="40 - 59 years old">40 - 59 years old</item>
-              <item value="20 - 39 years old">20 - 39 years old</item>
-              <item value="12 - 19 years old">12 - 19 years old</item>
-              <item value="0 - 11 years old">0 - 11 years old</item>
-            </select>
+            <label value="age_group">Select age group : </label>
+            <button value={age} onChange={(e) => setAge(e.target.value)}>
+              <button value="70 years old and above">70 years old and above</button>
+              <button value="60 - 69 years old">60 - 69 years old</button>
+              <button value="40 - 59 years old">40 - 59 years old</button>
+              <button value="20 - 39 years old">20 - 39 years old</button>
+              <button value="12 - 19 years old">12 - 19 years old</button>
+              <button value="0 - 11 years old">0 - 11 years old</button>
+            </button>
         </form> 
 
     </div>
--- src/screens/Covid.js
+++ src/screens/Covid.js
@@ -1,34 +1,45 @@
 import React from "react";
 import API from "../API";
-import AgeList from "../components/AgeList";
 
 
 class Covid extends React.Component {
   constructor() {
     super();
     this.getAge = this.getAge.bind(this);
+    this.getVacStatus = this.getVacStatus.bind(this);
 
     this.state = {
       age: [],
-      count: [],
+      vacStatus: [],
     };
   }
 
   componentDidMount() {
     this.getAge();
-    //this.getCount();
+    this.getVacStatus();
+    
   }
 
   async getAge() {
-      const res = await API.get("/api/action/datastore_search?resource_id=9de30d8d-3c0d-48ab-8c1b-4a7dc03d687a");
-      if (res.status === 200) {
-          console.log(res);
-          this.setState((state) => {
-              state.age = res.data.result.records;
-              //state.count = res.data.records;
-              return state;
-          })
-      }
+    const res = await API.get("/api/action/datastore_search?resource_id=6c14814b-09b7-408e-80c4-db3d393c7c15");
+    if (res.status === 200) {
+      console.log(res);
+      this.setState((state) => {
+        state.age = res.data.result.records;
+        return state;
+      });
+    }
+  }
+
+  async getVacStatus() {
+    const res = await API.get("/api/action/datastore_search?resource_id=60b1a923-1a5e-44ef-a4ae-046345146725");
+    if (res.status === 200) {
+      console.log(res);
+      this.setState((state) => {
+        state.vacStatus = res.data.result.records;
+        return state;
+      });
+    }
   }
 
   render() {
@@ -36,20 +47,33 @@ class Covid extends React.Component {
       <>
         <h1>Covid</h1>
         <div>
+          <h3>Number of local cases by Age</h3>
           {this.state.age.map((a) => {
             return (
               <div>
-                  <p>Count of Case : {a.count_of_case}</p>
-                  <p>Clinical Status : {a.clinical_status}</p>
-                  <p>Age Group : {a.age_groups}</p>
-                  <p>As of Date : {a.day_of_as_of_date}</p>
-                  <br />
-               </div>
+                <p>Count of Case : {a.count_of_case}</p>
+                <p>Age Group : {a.age_group}</p>
+                <p>As of Date : {a.pr_date}</p>
+                <br />
+              </div>
             );
           })}
 
-          <AgeList add={this.getAge}/>
-       </div>
+          <h3>Proportion of cases every requiring O2 in general ward, ICU or died, by age and vaccination status</h3>
+          {(this.state.vacStatus).map((v) => {
+            return (
+              <div>
+                
+                <p>Age Group : {v.age}</p>
+                <p>Fully Vaccinated without booster : {v.fully_vaccinated_without_booster}</p>
+                <p>Fully Vaccinated with booster : {v.fully_vaccinated_with_booster}</p>
+                <p>Non fully vaccinated : {v.non_fully_vaccinated}</p>
+
+                <br />
+              </div>
+            );
+          })} 
+        </div>
       </>
     );
   }
